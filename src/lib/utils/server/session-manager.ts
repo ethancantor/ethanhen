a0c6a9ac0c502ddd09e3f6811418dc2fd23522ad
Ethan Cantor <ethan.cantor99@gmail.com>
2025-07-16 20:17:59 -0400

    comment out session manager print statements

--- src/lib/utils/server/session-manager.ts
+++ src/lib/utils/server/session-manager.ts
@@ -12,15 +12,15 @@ export function createSession(): Session {
 	};
 	sessions.push(session);
 
-	console.log(
-		`Created session: ${session.id}, expires at: ${session.expiresAt}, ${JSON.stringify(sessions)}`
-	);
+	// console.log(
+	// 	`Created session: ${session.id}, expires at: ${session.expiresAt}, ${JSON.stringify(sessions)}`
+	// );
 
 	return session;
 }
 
 export function getSession(id: string): Session | undefined {
-	console.log(`Getting session for id: ${id}, current sessions: ${JSON.stringify(sessions)}`);
+	// console.log(`Getting session for id: ${id}, current sessions: ${JSON.stringify(sessions)}`);
 
 	const session = sessions.find((s) => s.id === id);
 	if (session && session.expiresAt > new Date()) {
@@ -31,7 +31,7 @@ export function getSession(id: string): Session | undefined {
 }
 
 export function deleteSession(id: string): void {
-	console.log(`Deleting session for id: ${id}, current sessions: ${JSON.stringify(sessions)}`);
+	// console.log(`Deleting session for id: ${id}, current sessions: ${JSON.stringify(sessions)}`);
 	const index = sessions.findIndex((s) => s.id === id);
 	if (index !== -1) {
 		sessions.splice(index, 1);
@@ -39,7 +39,7 @@ export function deleteSession(id: string): void {
 }
 
 export function validateSession(id: string): Session | undefined {
-	console.log(`Validating session for id: ${id}, current sessions: ${JSON.stringify(sessions)}`);
+	// console.log(`Validating session for id: ${id}, current sessions: ${JSON.stringify(sessions)}`);
 	const session = getSession(id);
 	if (session) {
 		session.isAdmin = true;
